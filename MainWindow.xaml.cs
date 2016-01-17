3185e39e90dd6b7dae15c6607c72ba07267bb156
NightBits <NightBits@gmail.com>
2016-01-17 13:52:07 +0100

    Added 2 more additional resolutions Changed resolution check Made improvements for (future) automatic resolution detection and patching

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,6 @@ namespace NightBitsMGSTPP_Resolution_Patcher
     public partial class MainWindow : Window
     {
         private byte[] _replace;
-        private bool _hasTest = false;
         private Dictionary<string, byte[]> _resolutionsDictionary = new Dictionary<string, byte[]>();
 
         public MainWindow()
@@ -24,7 +23,23 @@ namespace NightBitsMGSTPP_Resolution_Patcher
             InitializeComponent();
             FillInKnownResolutions();
             FillInComboBox();
-            GetAllValidResolutions();
+        }
+
+        private byte[] GetBytesArrayFromResolution(int resolutionX, int resolutionY)
+        {
+            var resultX = resolutionX.ToString("X4");
+            var resultY = resolutionY.ToString("X4");
+            byte[] resultXInBytes = StringToByteArray(resultX);
+            byte[] resultYInBytes = StringToByteArray(resultY);
+            return resultXInBytes.Concat(resultYInBytes).ToArray();
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
         }
 
         private void FillInComboBox()
@@ -38,6 +53,23 @@ namespace NightBitsMGSTPP_Resolution_Patcher
 
         private void FillInKnownResolutions()
         {
+            _resolutionsDictionary["1024X768 (4:3)"] = GetBytesArrayFromResolution(1024, 768);
+            _resolutionsDictionary["1280X1024 (5:4)"] = GetBytesArrayFromResolution(1280, 1024);
+            _resolutionsDictionary["1366X768 (16:9)"] = GetBytesArrayFromResolution(1366, 768);
+            _resolutionsDictionary["1440X900 (16:10)"] = GetBytesArrayFromResolution(1440, 900);
+            _resolutionsDictionary["1680X1050 (16:10)"] = GetBytesArrayFromResolution(1680, 1050);
+            _resolutionsDictionary["1920X1080 (16:9)"] = GetBytesArrayFromResolution(1920, 1080);
+            _resolutionsDictionary["2560X1080 (21:9)"] = GetBytesArrayFromResolution(2560, 1080);
+            _resolutionsDictionary["3440X1440 (21:9)"] = GetBytesArrayFromResolution(3440, 1440);
+            _resolutionsDictionary["5040x1050 (48:10)"] = GetBytesArrayFromResolution(5040, 1050);
+            _resolutionsDictionary["4800X900 (48:9)"] = GetBytesArrayFromResolution(4800, 900);
+            _resolutionsDictionary["7860X1080 (48:9)"] = GetBytesArrayFromResolution(7860, 1080);
+            _resolutionsDictionary["7860X2160 (48:9)"] = GetBytesArrayFromResolution(7860, 2160);
+        }
+
+        /* // Is not used anymore
+        private void OldKnownResolutions()
+        {
             _resolutionsDictionary["1024X768 (4:3)"] = new byte[] { 0xAB, 0xAA, 0xAA, 0x3F };
             _resolutionsDictionary["1280X1024 (5:4)"] = new byte[] { 0x00, 0x00, 0x0A, 0x3F };
             _resolutionsDictionary["1366X768 (16:9)"] = new byte[] { 0xAB, 0xAA, 0xE3, 0x3F };
@@ -45,11 +77,14 @@ namespace NightBitsMGSTPP_Resolution_Patcher
             _resolutionsDictionary["1680X1050 (16:10)"] = new byte[] { 0xCD, 0xCC, 0xCC, 0x3F };
             _resolutionsDictionary["1920X1080 (16:9)"] = new byte[] { 0x39, 0x8E, 0xE3, 0x3F };
             _resolutionsDictionary["2560X1080 (21:9)"] = new byte[] { 0x26, 0xB4, 0x17, 0x40 };
-           _resolutionsDictionary["3440X1440 (21:9)"] = new byte[] { 0x8E, 0xE3, 0x18, 0x40 };
-           _resolutionsDictionary["5040x1050 (48:10)"] = new byte[] { 0x9A, 0x99, 0x99, 0x40 };
-           _resolutionsDictionary["4800X900 (48:9)"] = new byte[] { 0xBA, 0xAA, 0xAA, 0x40 };
+            _resolutionsDictionary["3440X1440 (21:9)"] = new byte[] { 0x8E, 0xE3, 0x18, 0x40 };
+            _resolutionsDictionary["5040x1050 (48:10)"] = new byte[] { 0x9A, 0x99, 0x99, 0x40 };
+            _resolutionsDictionary["4800X900 (48:9)"] = new byte[] { 0xBA, 0xAA, 0xAA, 0x40 };
+            _resolutionsDictionary["7860X1080 (48:9)"] = new byte[] { 0xBA, 0xAA, 0xAA, 0x40 };
         }
+        */
 
+        // Doesnt work yet
         private void GetAllValidResolutions()
         {
             var scope = new ManagementScope();
@@ -61,63 +96,32 @@ namespace NightBitsMGSTPP_Resolution_Patcher
                 var results = searcher.Get();
 
                 //previous resolution for checking when new resolution is found
-                string prevReso = string.Empty;
+                string previousResolution = string.Empty;
                 //current resolution
-                string currReso = string.Empty;
+                string currentResolution = string.Empty;
 
                 foreach (var result in results)
                 {
-                    currReso = result["HorizontalResolution"] + "x" + result["VerticalResolution"];
+                    currentResolution = result["HorizontalResolution"] + "x" + result["VerticalResolution"];
 
-                    if (currReso != prevReso)
+                    if (currentResolution != previousResolution)
                     {
-                        comboBox.Items.Add(currReso);
-                        prevReso = currReso;
+                        comboBox.Items.Add(currentResolution);
+                        previousResolution = currentResolution;
                     }
                 }
             }
         }
 
-        private static string ConvertToHex(byte[] bytes)
-        {
-            var builder = new StringBuilder();
-
-            var hexCharacters = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-
-            for (var i = 0; i < bytes.Length; i++)
-            {
-                int firstValue = (bytes[i] >> 4) & 0x0F;
-                int secondValue = bytes[i] & 0x0F;
-
-                char firstCharacter = hexCharacters[firstValue];
-                char secondCharacter = hexCharacters[secondValue];
-
-                builder.Append("0x");
-                builder.Append(firstCharacter);
-                builder.Append(secondCharacter);
-                builder.Append(' ');
-            }
-
-            return builder.ToString().Trim(' ');
-        }
-
         private void PatchButton_Click(object sender, RoutedEventArgs e)
         {
-            if (_hasTest)
+            if (comboBox.Text.Length > 0)
             {
-                uint test = 3440 + 1440;
-                MessageBox.Show(ConvertToHex(Encoding.UTF8.GetBytes(test.ToString())));
+                PatchFile();
             }
             else
             {
-                if (comboBox.Text.Length > 0)
-                {
-                    PatchFile();
-                }
-                else
-                {
-                    MessageBox.Show("Please select a resolution!");
-                }
+                MessageBox.Show("Please select a resolution!");
             }
         }
 
@@ -160,20 +164,6 @@ namespace NightBitsMGSTPP_Resolution_Patcher
                 }
                 MessageBox.Show("Patcher created by NightBits");
             }
-        }
-
-        public string Converts(double d)
-        {
-            float f = (float)d;
-            byte[] convertToBytes = new byte[32];
-            string hex = "";
-            convertToBytes = BitConverter.GetBytes(f);
-            for (int i = (convertToBytes.Count(x => x != 0)) - 1; i >= 0; i--)
-            {
-                hex = hex + "," + convertToBytes[i].ToString("X");
-            }
-
-            return hex;
         }
 
         private void CreateBackup(string filename)
